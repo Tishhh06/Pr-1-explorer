7f0c320a83a99040dee8b209762cb35dcb1242f3
Tishhh06 <164470925+Tishhh06@users.noreply.github.com>
2024-12-24 23:44:53 +0530

    Add files via upload

---
+++ Project1-Explorer-main/src/components/Component1.jsx
@@ -0,0 +1,9 @@
+function Component1() {
+    return (
+      <>
+          <h2>Hello from com 1</h2> 
+      </>
+    )
+  }
+  
+  export default Component1
---
+++ Project1-Explorer-main/src/components/Component2.jsx
@@ -0,0 +1,9 @@
+function Component2() {
+    return (
+      <>
+          <h2>Hello From com 2</h2> 
+      </>
+    )
+  }
+  
+  export default Component2
